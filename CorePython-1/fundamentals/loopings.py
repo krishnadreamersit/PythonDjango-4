168f96af2412fd30b5061beacf119248f75cce7a
Krishna Aryal <krishna.dreamersit@gmail.com>
2022-01-23 09:25:39 +0545

    Jan 23, 2022

---
+++ CorePython-1/fundamentals/loopings.py
@@ -0,0 +1,9 @@
+# Looping Statements
+
+# 1. While Loop
+# print 1 to 5
+startno=1   # Start No -1
+stopno=5    # Stop No -2
+while(startno<=stopno): # Condition -3
+    print(startno) # Statement(s) -4
+    startno+=1 # Modifier -5
---
+++ CorePython-1/fundamentals/selections.py
@@ -0,0 +1,8 @@
+# Selection Statements
+# if statement
+num1 = 0
+if num1 == 0:  # Condition (==, !=, >, >=, <, <=)
+    print("TRUE")
+else:
+    print("FALSE")
+
