1cfb095f6d2c7aa219b4a0389ff119217beeac34
Krishna Aryal <krishna.dreamersit@gmail.com>
2022-04-04 07:09:22 +0545

    April 4, 2022

--- MySite402/app2_1/admin.py
+++ MySite402/app2_1/admin.py
@@ -3,8 +3,18 @@ from app2_1.models import Person # Step-1
 from app2_1.models import Person2
 from app2_1.models import Country
 
+from app2_1.models import Model1
+from app2_1.models import Model2
+
+from app2_1.models import Place
+from app2_1.models import Restaurant
+
 # Register your models here.
 
 admin.site.register(Person) # Step-2
 admin.site.register(Person2)
 admin.site.register(Country)
+admin.site.register(Model1)
+admin.site.register(Model2)
+admin.site.register(Place)
+admin.site.register(Restaurant)
--- MySite402/app2_1/models.py
+++ MySite402/app2_1/models.py
@@ -29,3 +29,61 @@ class Country(models.Model):
 
     def __str__(self):
         return str(self.cid)+", "+self.country+", "+self.region+", "+ str(self.population)+", "+str(self.area)
+
+# Models
+class Model1(models.Model):
+    # automatically add id field as primary key and autoincrement
+    fullName = models.CharField(max_length=50)
+
+    class Meta:
+        #ordering=['id'] # ASC
+        ordering = ['-id']  # DESC
+        # default table name : app2_1_Model1
+        # db_table = 'tbl_persons'
+
+    # set visibility of model in admin panel
+    def __str__(self):
+        return str(self.id)+", "+self.fullName
+
+class Model2(models.Model):
+    pid = models.IntegerField(primary_key=True) # User defined primary key, manually assign value
+    fullName = models.CharField(max_length=50)
+    contactAddress = models.CharField(max_length=50)
+
+class Model3(models.Model):
+    pass
+
+# Models field types
+# https://docs.djangoproject.com/en/4.0/ref/models/fields/
+
+# Relationship between Models
+
+# One to One
+class Place(models.Model):
+    name = models.CharField(max_length=50, help_text="Enter name of place")
+    def __str__(self):
+        return str(self.id)+", "+self.name
+
+class Restaurant(models.Model):
+    place = models.OneToOneField(Place, primary_key=True, on_delete=models.CASCADE, help_text="Select Location")
+    name = models.CharField(max_length=50, help_text="Enter Restaurent Name ")
+    momo = models.BooleanField(default=True, help_text="Serve MoMo")
+
+    def __str__(self):
+        return str(self.place)+", "+self.name+", "+str(self.momo)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
